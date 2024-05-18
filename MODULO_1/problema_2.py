ab727295fcebe54731a5a719f15df43e0e6bdd23
Familia Wolff Casanova <fwc@iMac-de-Familia.local>
2024-05-18 14:14:05 -0400

    problema 2

--- MODULO_1/problema_2.py
+++ MODULO_1/problema_2.py
@@ -1,3 +1,13 @@
 '''
 EL usuario ingresa un numero, el programa debe decir si es par o impar
 '''
+
+a = input('por favor ingrese un numero: ')
+a= int(a)
+
+sol = a%2
+
+if sol == 0:
+    print(a, 'es par.')
+elif sol != 0:
+    print(a, 'es impar.')
