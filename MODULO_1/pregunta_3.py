a265af758bb41c2c94b60aec6be53fa080bcc2a8
LukasWolff2002 <lwolff@miuandes.cl>
2024-05-18 14:41:40 -0400

    pregunta 3

---
+++ MODULO_1/pregunta_3.py
@@ -0,0 +1,16 @@
+'''
+PROBLEMA 3:
+
+Son tiempos de guerra, por lo que, para que no se puedan desifrar los mensajes se envian las letras en forma de numeros segun tabla ASCII. 
+Debes hacer un codigo el cual permita enviar una oracion de 10 caracteres usando solo 1 input escrito en toooooooodo el codigo. 
+Este input debe recibir un numero y tu debes desifrar ese numero a letra. Al final entregar la oracion completa.
+
+Hint: investigar la tabla ASCII y como se implementa en python
+'''
+
+#Recordar que
+
+caracter_1 = 'h'
+caracter_2 = '0'
+
+print(caracter_1+caracter_2)
